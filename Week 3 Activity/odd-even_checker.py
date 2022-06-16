98058788a1771081850f91585c4bef4a91cb8e7c
Orion Fernandes <80146094+orionfernandes@users.noreply.github.com>
2022-06-16 15:40:14 -0700

    odd-even_checker.py
    Create a program that takes in a list and outputs the reverse order of elements in a list.
    
    Examples - 
    Input: [“blue”, “yellow”, “green”, “red”]
    Output:[“red”, “green”, “yellow”, “blue”] 
    
    Input: [0, 1, 2]
    Output: [2, 1, 0]
    
    Input: ["hello"]
    Output: ["hello"]
    
    Input: []
    Output: []
    -

---
+++ Week 3 Activity/odd-even_checker.py
@@ -0,0 +1,30 @@
+list = []
+
+inp = input("Enter numbers to list: (n to stop): ")
+list.append(inp)
+
+
+def count_eo(list):
+	e = 0  	# even counter set to 0
+	o = 0	# odd counter set to 0
+	for i in range(len(list)):
+		if i%2 ==0:
+			e += 1		# even counter increment
+		else:
+			o+= 1		# odd counter increment
+
+	print("Number of even numbers in the list: ", e)
+	print("Number of odd numbers in the list: ", o)
+
+
+
+while (inp!='n'):
+	inp = input("Enter numbers to list -  (n to stop): ")
+	list.append(inp)
+	
+	
+if inp == 'n':
+	list.pop(-1)
+	print("Your list is: ", list)
+	count_eo(list)
+
