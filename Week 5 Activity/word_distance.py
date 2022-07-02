4ec5fe9d258e512e21a1f4eef663afa1bfc1de94
Orion Fernandes <80146094+orionfernandes@users.noreply.github.com>
2022-07-02 11:05:58 -0700

    Update word_distance.py

--- Week 5 Activity/word_distance.py
+++ Week 5 Activity/word_distance.py
@@ -1,16 +1,17 @@
-s = ["the", "quick", "brown", "fox", "quick"]
+word_list = ["cat", "dog", "abcd", "dog", "cat", "cat", "abcd", "dog", "wxyz"]
 
-word1 = "the"
+word1 = "abcd"
 
-word2 = "fox"
+word2 = "wxyz"
 
-def word_distance(word_list, word1, word2):
-	
-	for i in range(len(s)):
-		for j in range(len(s)):
-			if s[i] == word1 and s[j] == word2:
-				answer = s.index(word2) - s.index(word1)
+def solve(word_list, word1, word2):
+	ans = len(word_list)
+	L = None
+	for R in range(len(word_list)):
+		if word_list[R] == word1 or word_list[R] == word2:
+			if L is not None and word_list[R] != word_list[L]:
+				ans = min(ans, R - L)
+			L = R
+	return -1 if ans == len(word_list) else ans
 
-	print(answer)
-
-word_distance(s, word1, word2)
+solve(word_list, word1, word2)
