f63423d279e9e3add63e5f87d65f0f1782d24adb
Orion Fernandes <80146094+orionfernandes@users.noreply.github.com>
2022-07-13 15:58:06 -0700

    slidingwindow_max.py
    Goal:  Given a list lst and an integer w, find the maximum for each and every contiguous sublist of size w.

---
+++ Week 7 Activity/slidingwindow_max.py
@@ -0,0 +1,26 @@
+
+lst = [1, 2, 3, 1, 4, 5]
+w = 3
+
+def find_substring(lst, w):
+	ptr1 = 0
+	ptr2 = w
+	maxi = 0
+	lists =[]
+	while ptr2 <= len(lst):
+		if max(lst[ptr1:ptr2]) > maxi:
+			maxi = max(lst[ptr1:ptr2])
+			lists.append(maxi)
+		else:
+			maxi = maxi
+			lists.append(maxi)
+
+		ptr1 += 1
+		ptr2 += 1
+
+	print(lists)	
+
+
+	print(", ".join(map(str,lists)))
+
+find_substring(lst, w)
